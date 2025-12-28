4f277b162628c31c9909cb7ecb5c29ede73d9643
Sumit Sheoran <sumitsheoran642@gmail.com>
2025-12-28 21:48:57 +0530

    Update script logic

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let divs = document.querySelectorAll(".box");
 console.dir(divs);
 
-divs[0].innerText = "helou my name is Sumit";
-divs[1].innerText = "is that a lion";
-divs[2].innerText = "yes I am a don";
+let idx = 1;
+for(div of divs){
+    div.innerText = `unique vale ${idx}`;
+    idx++;
+}
+
 
