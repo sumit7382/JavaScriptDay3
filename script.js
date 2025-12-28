3792af0331739dbc425266ee73bd22e6a7d44b90
Sumit Sheoran <sumitsheoran642@gmail.com>
2025-12-28 21:38:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let divs = document.querySelectorAll(".box");
+console.dir(divs);
+
+divs[0].innerText = "helou my name is Sumit";
+divs[1].innerText = "is that a lion";
+divs[2].innerText = "yes I am a don";
+
